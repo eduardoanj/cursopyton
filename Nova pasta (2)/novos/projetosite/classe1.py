85951097270075ffec475b38d1d789d7602d51d9
Edu <eduardo_anjos15@yahoo.com>
2019-10-09 17:12:09 -0300

    aulas

---
+++ Nova pasta (2)/novos/projetosite/classe1.py
@@ -0,0 +1,6 @@
+class heroe:
+    def __init__(self, name, damage, speed, peel):
+        self.name = name
+        self.damage = damage
+        self.speed = speed
+        self.peel = peel
--- Nova pasta (2)/novos/projetosite/principal.py
+++ Nova pasta (2)/novos/projetosite/principal.py
@@ -1,12 +1,21 @@
 from flask import Flask, render_template, request
 
-app = Flask
+
+
+app = Flask(__name__)
+
 
 p_nome = 'HOTS- Heroes of the Storm'
 
+titulo_pagina = 'HOTS FLAGS'
+
 @app.route('/')
 def princ():
-    return ('index.html')
+    return render_template('index.html')
+
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
 
 
 
